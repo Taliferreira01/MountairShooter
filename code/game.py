24b4237039dd1898c76474ffbd56b368e1a3bb9d
TalitaVitoria <talita.vitoria.1997@hotmail.com>
2024-08-13 22:51:39 -0300

    create main loop Conecting Menu and Game classes

--- code/game.py
+++ code/game.py
@@ -1,9 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame
+
+from code.menu import Menu
+
 
 class Game:
     def __init__(self):
-        self.window = None
+       pygame.init()
+       self.window = pygame.display.set_mode(size=(600, 480))
 
-    def run(self, ):
-        pass
+    def run(self):
+
+        while True:  # laço de repetição mantém a janela aberta
+            menu = Menu(self.window)
+            menu.run()
+            pass
+
+
+
+            # check dor all events       evento para usar o fechar dp pygame
+            #for event in pygame.event.get():
+             #   if event.type == pygame.QUIT:
+              #      pygame.quit()  # close window
+               #     quit()  # end pygame.
--- main.py
+++ main.py
@@ -1,17 +1,4 @@
-# bem vindo ao python
-import pygame
-import pygame as pg
+from code.game import Game
 
-print('Setap start')
-pygame.init()
-# criar janela vamos criar uma variável chamada
-window = pygame.display.set_mode(size=(600, 480))
-print('Setup End')
-
-print('Start loop')
-while True:  # laço de repetição mantém a janela aberta
-    # check dor all events       evento para usar o fechar dp pygame
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()  # close window
-            quit()  # end pygame.
+game= Game()
+game.run()
