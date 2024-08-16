cd5fecc15ad09743d56e17fdb0b72a4cab2a3ed7
TalitaVitoria <talita.vitoria.1997@hotmail.com>
2024-08-16 17:57:21 -0300

    Add Menu, move image, level 1

--- code/level.py
+++ code/level.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import pygame.display
+
+from code.entity1 import Entity1
+from code.entityFactory import EntityFactory
+
 
 class Level:
-    def __init__(self):
-        self.window = None
-        self.name = None
-        self.entity_list = None
+    def __init__(self, window, name, game_mode):
+        self.window = window
+        self.name = name
+        self.game_mode = game_mode
+        self.entity_list: list[Entity1] = []
+        self.entity_list.extend(EntityFactory.get_entity('Level1Bg')) #pegar obj lista e jogar aqui dentro
 
-    def run(self, ):
+    def run(self):
+        while True:
+            for ent in self.entity_list:
+                self.window.blit(source=ent.surf, dest=ent.rect)
+                ent.move() # invocar o movimento
+            pygame.display.flip()
         pass
--- code/menu.py
+++ code/menu.py
@@ -4,30 +4,51 @@ import pygame.image
 from pygame import Surface, Rect
 from pygame.font import Font
 
-from code.Const import WIN_WIDTH, COLOR_ORANGE
+from code.Const import WIN_WIDTH, C_ORANGE, MENU_OPTION, C_YELLOW, C_WHITE
 
 
 class Menu:
     def __init__(self, window):
         self.window = window
-        self.surf = pygame.image.load('./asset/bg.png') #carregou a imagem
+        self.surf = pygame.image.load('./Asset/bg.png') #carregou a imagem
         self .rect = self.surf.get_rect(left=0, top=0) # criou o retangulo
 
 
-    def run(self,):
-        pygame.mixer_music.load('./asset/fase1.mp3')
+    def run(self):
+        menu_option = 0
+        pygame.mixer_music.load('./asset/Menu.mp3')
         pygame.mixer_music.play(-1)
+
         while True:
             self.window.blit(source=self.surf, dest=self.rect)  # imagem tem que aparecer no retang
-            self.menu_text(50,"Mountain", COLOR_ORANGE, ((WIN_WIDTH / 2 ), 70))
-            self.menu_text(50, "Shooter",COLOR_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(50,"Mountain", C_ORANGE, ((WIN_WIDTH / 2 ), 70))
+            self.menu_text(50, "Shooter",C_ORANGE, ((WIN_WIDTH / 2), 120))
+
+            for i in range(len(MENU_OPTION)):
+                if i == menu_option:
+                    self.menu_text(20, MENU_OPTION[i], C_YELLOW, ((WIN_WIDTH / 2), 200 + 25 * i))
+                else:
+                    self.menu_text(20, MENU_OPTION[i], C_WHITE, ((WIN_WIDTH / 2), 200 + 25 * i))
             pygame.display.flip()  # atualizar na tela
 
-            # check dor all events       evento para usar o fechar dp pygame
+            # check dor all events       evento para usar o fechar dp pygame e comando teclas
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()  # close window
                     quit()  # end pygame.
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_DOWN:  # DOWN KEY
+                        if menu_option < len(MENU_OPTION) - 1:
+                            menu_option += 1
+                        else:
+                            menu_option = 0
+                    if event.key == pygame.K_UP:  # UP KEY
+                        if menu_option > 0:
+                            menu_option -= 1
+                        else:
+                            menu_option = len(MENU_OPTION) - 1
+                    if event.key == pygame.K_RETURN:  # ENTER
+                        return MENU_OPTION[menu_option]
     def menu_text(self,text_size: int, text: str, text_color: tuple, text_center_pos: tuple):
         text_font: Font = pygame.font.SysFont(name="Lucida Sans Typewriter", size=text_size)
         text_surf: Surface = text_font.render(text, True, text_color).convert_alpha()
